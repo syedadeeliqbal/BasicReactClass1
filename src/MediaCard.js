bf95fc041f1e5f9c9d64b3650f71239baf748a31
syedadeeliqbal <syedadeeliqbal@gmail.com>
2020-06-06 23:13:00 -0400

    Removing Image word from Alt Image

--- src/MediaCard.js
+++ src/MediaCard.js
@@ -4,7 +4,7 @@ function MediaCard({title, body, imageUrl}) {
   return <div>
       <h2>This {title} is the title of the Media Card in h2 tag</h2>
       <p>This is the {body} in p tag</p>
-      <img alt="Test Image" src={imageUrl}/>
+      <img alt="F16" src={imageUrl}/>
 
   </div>
 }
